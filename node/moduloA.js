1538d4e80255279fe76c8e419a22dce928901978
Daniel Franca <danielfranca.tech@gmail.com>
2022-10-28 16:54:54 -0300

    commit

---
+++ node/moduloA.js
@@ -0,0 +1,3 @@
+this.ola = 'fala pessoa'
+exports.bemVindo = 'bem vindo ao node'
+module.exports.ateLogo = 'até o próximo exemplo'
---
+++ node/moduloCliente.js
@@ -0,0 +1,11 @@
+const moduloA = require('./moduloA')  //Caminho relativo -- caminho que se encontra dentro da pasta
+const moduloB = require('./moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)
