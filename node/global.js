595196588270b58f747bf4c9ffb29050979598ec
Daniel Franca <danielfranca.tech@gmail.com>
2022-10-31 15:06:54 -0300

    commit

---
+++ node/global.js
@@ -0,0 +1,7 @@
+//console.log(global)
+globalThis.MinhaApp = Object.freeze({ //objeto congelado = sem alterações = constante = imutavel
+    saudacao(){
+        return 'estou em todos os lugares!'
+    },
+    nome: 'sistema legal'
+})
---
+++ node/globalCliente.js
@@ -0,0 +1,6 @@
+require('./global') //fica disponivel na aplicação inteira
+
+console.log(MinhaApp.saudacao())
+
+MinhaApp.nome = 'eita'
+console.log(MinhaApp.nome)
